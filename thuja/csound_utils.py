036d9686d878c3984e08d9c4bcdb48fa81b65433
Ben McAllister <ben@listenfaster.com>
2022-05-17 15:53:59 -0700

    Added play_csound function back in, for bkwds compatibility with some thujas

--- thuja/csound_utils.py
+++ thuja/csound_utils.py
@@ -1,5 +1,6 @@
 import ctcsound
 
+
 def init_csound_with_orc(args_list, orc_file, silent, string_values):
     with open(orc_file, "r") as f:
         orc_string = f.read()
@@ -30,3 +31,23 @@ def init_csound_with_csd(args_list, csd_file, silent, string_values):
         for k in string_values.keys():
             cs.setStringChannel(k, string_values[k])
     return cs
+
+
+def play_csound(orc_file, generator, args_list=['-odac', '-W'], string_values=None, silent=False):
+    with open(orc_file, "r") as f:
+        orc_string = f.read()
+    if silent:
+        args_list.append("-m0")
+        args_list.append("-d")
+    score_string = generator.generate_score_string()
+    cs = init_csound_with_orc(args_list, orc_file, silent, string_values)
+    cs.compileOrc(orc_string)
+    cs.readScore(score_string)
+    for x in args_list:
+        cs.setOption(x)
+    if string_values:
+        for k in string_values.keys():
+            cs.setStringChannel(k, string_values[k])
+    cs.start()
+    cs.perform()
+    cs.stop()
